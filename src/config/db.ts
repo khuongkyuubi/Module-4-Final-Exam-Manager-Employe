01e39f0a82bd24a884ab64b176cc3872a97d11f7
khuongkyuubi <khuong.kyuuib@gmail.com>
2022-07-20 11:39:55 +0700

    update

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 
-const DB_URI = "mongodb://localhost:27017/dbTest";
+// const DB_URI = "mongodb://localhost:27017/dbTest";
+const DB_URI = "mongodb+srv://khuongkyuubi:mongodb123456@posts.meidn.mongodb.net/dbTest?retryWrites=true&w=majority"
 const connectDB = async function () {
     try {
         await mongoose.connect(DB_URI, {
